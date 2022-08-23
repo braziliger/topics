9d7cc33ac65db2d414c4252c65b8629555536032
braziliger <cmaxey87@gmail.com>
2022-08-22 23:44:35 -0500

    Add stuff

---
+++ components/HTML/header.php
@@ -0,0 +1,20 @@
+<body>
+    <header>
+        <div class="content">
+            <hgroup>
+                <h1>LOGO</h1>
+                <i>slogan</i>
+            </hgroup>
+        </div>
+        <div class="overlay"></div>
+    </header>
+    <section class="site">
+        <nav>
+            <a href="">Page</a>
+            <a href="">Page</a>
+            <a href="">Page</a>
+            <a href="">Page</a>
+            <a href="">Page</a>
+        </nav>
+    </section>
+</body>
---
+++ testpage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./components/CSS/header.css">
+    <script src="./components/JS/header.js"></script>
+    <title>Info </title>
+</head>
+<body>
+<?php include_once('./components/HTML/header.php'); ?>X
+
+</body>
+</html>
